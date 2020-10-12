d21975dfb7154263cf75b86fb92abdcb93a8aeb2
efficacy38 <60431808+efficacy38@users.noreply.github.com>
2020-10-12 16:08:05 +0800

    add comment on 10071.cpp
    幫 10071 增加註解

--- 一星題/10071/10071.cpp
+++ 一星題/10071/10071.cpp
@@ -3,16 +3,12 @@ https://uva.onlinejudge.org/index.php?Itemid=8&category=12&option=com_onlinejudg
 using namespace std;
 int main()
 {
-  int v,t;                                  //物理的v和t
-  while(cin>>v>>t)                          //輸入v和t
+  int v,t;                                  // 物理的 v 和 t
+  while(cin>>v>>t)                          // 輸入 v 和 t
   {
     if(-100<=v && v<=100 && 0<=t && t<=200)
     {
-      cout<<v*2*t<<endl;                  //速度公式 H = v * t，題目說輸出2倍時間可以跑的距離
+      cout<<v*2*t<<endl;                  //速度公式 H = v * t，題目說輸出 2 倍時間可以跑的距離
     }
   }
 }
-
-
-¡°¬d¸ß¨ä¥LCPEÃD¥Ø¦p¤U¡°
-http://jennaweng0621.pixnet.net/blog/category/6372301
