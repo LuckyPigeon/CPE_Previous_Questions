f522c5e9abe9633eee19831c1746239e13f30183
Min Tang <47917301+MinTimmy@users.noreply.github.com>
2020-10-16 20:11:39 +0800

    Update 10055.cpp
    新增新註解

--- 一星題/10055/10055.cpp
+++ 一星題/10055/10055.cpp
@@ -1,18 +1,17 @@
-https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=996
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 int main()
 {
-  long long int Hashmat, opponent;
+  
+  // è¼¸å…¥å…©å€‹ long long å¾—æ•´æ•¸
+  long long int Hashmat, opponent;// å®£å‘Šå…©å€‹é•·æ•´æ•¸ Hashmat ï¼Œopponent
+  // ç”¨ while è¿´åœˆè®“ä½¿ç”¨è€…é€£çºŒè¼¸å…¥ 
   while(cin>>Hashmat>>opponent)
   {
+    // æ•¸å‡º Hashmat å’Œ opponent å·®çš„çµ•å°å€¼
     cout<<abs(Hashmat-opponent)<<endl;
   }
 }
 
 
-¡°¬d¸ß©Ò¦³ÃD¥Ø¤Î¸Ñµª¦p¤U¡°
-https://jennaweng0621.pixnet.net/blog/post/403629785-CPE%E9%A1%8C%E7%9B%AE%E7%9B%AE%E9%8C%84%E5%8F%8A%E8%A7%A3%E7%AD%94
-¡°¬d¸ß¨ä¥LCPEÃD¥Ø¦p¤U¡°
-http://jennaweng0621.pixnet.net/blog/category/6372301
