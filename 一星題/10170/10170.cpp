57bc2aecdb8351b6819ff84113816eeebcae7c90
Lisa304 <61690790+Lisa304@users.noreply.github.com>
2020-10-17 00:08:33 +0800

    Update 10170.cpp

--- 一星題/10170/10170.cpp
+++ 一星題/10170/10170.cpp
@@ -13,13 +13,13 @@ using namespace std;
 int main()
 {
  long long int S,D;　//　接收兩個值，因為很大，所以資料型態用long long int
- long long int area;　//　算第n個團體離開的那天
+ long long int area;　//　算第　n　個團體離開的那天
  while(cin >> S >> D) {　//　當有兩個輸入就一直執行
    for(int i = 0; ; i++) {
     area = (S + (S + i)) * (i + 1) / 2;　//　上底加下底乘高除二，可以算出第　s　+　i　個人的團體要離開的那一天
     if(area >= D) {　//　當　area　>=　D　，表示第　D　天是在　s　+　i　個人團體的住宿期間
       cout << S + i << endl;
-      break;　//　這個for迴圈並沒有放結束的條件，當得到答案時就要跳出該迴圈
+      break;　//　這個　for　迴圈並沒有放結束的條件，當得到答案時就要跳出該迴圈
     }
    }
  }
