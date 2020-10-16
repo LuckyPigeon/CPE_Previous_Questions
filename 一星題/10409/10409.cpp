db48ad7aefdf874dc3154c2a08e2607999c6ca3f
Min Tang <47917301+MinTimmy@users.noreply.github.com>
2020-10-16 20:38:31 +0800

    Update 10409.cpp
    新增註解

--- 一星題/10409/10409.cpp
+++ 一星題/10409/10409.cpp
@@ -4,16 +4,22 @@ https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&probl
 #include <string>
 using namespace std;
 int main() {
- int m;
+ int m;// 儲存使用者將要給予指令的數量
+ // 連續輸入 m
  while (cin >> m){
+  // 如果 m 小於 0 結束程式
   if (m <= 0)
    break;
-  int u = 1, n = 2, w = 3, e = 4, s = 5, o = 6;
+  
+  int u = 1, n = 2, w = 3, e = 4, s = 5, o = 6; // 骰子的初始狀態 上(u = 1)，北(n = 3)，西(w = 3)，下(o = 6)，東(e = 4)，南(s = 5)
+  // 逐一輸入指令
   for (int i = 0; i < m; i++){
-   string stg;
-   int t;
-   cin >> stg;
+   string stg; // 宣告字串 stg ，用來存取使用者所輸入的指令
+   int t; // 宣告整數 t ，用來暫存指令
+   cin >> stg; // 數入指令
+   // 判斷指令的第一個字元 ， 並改變骰子上下東西南北的數字
    switch (stg[0]){
+    // 如果是 n 
     case 'n':
      t = u;
      u = s;
@@ -21,6 +27,7 @@ int main() {
      o = n;
      n = t;
      break;
+    // 如果是 s 
     case 's':
      t = u;
      u = n;
@@ -28,6 +35,7 @@ int main() {
      o = s;
      s = t;
      break;
+    // 如果是 e
     case 'e':
      t = u;
      u = w;
@@ -35,6 +43,7 @@ int main() {
      o = e;
      e = t;
      break;
+    // 如果是 w
     case 'w':
      t = u;
      u = e;
@@ -44,7 +53,7 @@ int main() {
      break;
    }
   }
-  cout << u << endl;
+  cout << u << endl; // 輸出骰子上方最終的數字
  }
  return 0;
 }
