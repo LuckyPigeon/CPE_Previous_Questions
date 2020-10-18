0f1d5b53ff3a74f6cbb1cac9c64cb44f1d09685d
Min Tang <47917301+MinTimmy@users.noreply.github.com>
2020-10-18 10:57:16 +0800

    Update 10055.cpp

--- 一星題/10055/10055.cpp
+++ 一星題/10055/10055.cpp
@@ -1,10 +1,10 @@
-#include <iostream> /* */
-#include <cstdlib> /* */
-using namespace std; /* */
+#include <iostream> 
+#include <cstdlib> 
+using namespace std; 
 int main()
 {
-  long long int Hashmat, opponent;// 宣告兩個長整數 Hashmat，opponent，因為資料的大小有可能超過 2^32，所以要用 long long int 存取。
-  // 用 while 迴圈讓使用者連續輸入 
+  long long int Hashmat, opponent;// 宣告兩個長整數 Hashmat、opponent。因為資料的大小有可能超過 2^32，所以要用 long long int 存取。
+  /* 用 while 迴圈讓使用者連續輸入 */
   while(cin>>Hashmat>>opponent)
   {
     cout<<abs(Hashmat-opponent)<<endl;// 數出 Hashmat 和 opponent 差的絕對值
