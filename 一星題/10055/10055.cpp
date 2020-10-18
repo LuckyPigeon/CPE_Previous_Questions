d923fbb5584dc0115edd38fab61644af8592a07d
Min Tang <47917301+MinTimmy@users.noreply.github.com>
2020-10-18 23:34:32 +0800

    Update 10055.cpp (#43)
    * Update 10055.cpp
    
    新增新註解
    
    * Update 10055.cpp
    
    * Update 10055.cpp
    
    * Update 10055.cpp
    
    Cleaning comment format
    
    Co-authored-by: Pigeon <32315294+LuckyPigeon@users.noreply.github.com>

--- 一星題/10055/10055.cpp
+++ 一星題/10055/10055.cpp
@@ -1,18 +1,14 @@
-https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=996
-#include <iostream>
-#include <cstdlib>
-using namespace std;
+#include <iostream> 
+#include <cstdlib> 
+using namespace std; 
 int main()
 {
-  long long int Hashmat, opponent;
+  long long int Hashmat, opponent; // 宣告兩個長整數 Hashmat、opponent。因為資料的大小有可能超過 2^32，所以要用 long long int 存取。
+  /* 用 while 迴圈讓使用者連續輸入 */
   while(cin>>Hashmat>>opponent)
   {
-    cout<<abs(Hashmat-opponent)<<endl;
+    cout<<abs(Hashmat-opponent)<<endl; // 數出 Hashmat 和 opponent 差的絕對值
   }
 }
 
 
-���d�ߩҦ��D�ؤθѵ��p�U��
-https://jennaweng0621.pixnet.net/blog/post/403629785-CPE%E9%A1%8C%E7%9B%AE%E7%9B%AE%E9%8C%84%E5%8F%8A%E8%A7%A3%E7%AD%94
-���d�ߨ�LCPE�D�ئp�U��
-http://jennaweng0621.pixnet.net/blog/category/6372301
