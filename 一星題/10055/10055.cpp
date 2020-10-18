fb416173dc48f8b486b354758800787235a49257
Pigeon <32315294+LuckyPigeon@users.noreply.github.com>
2020-10-18 23:30:10 +0800

    Update 10055.cpp
    Cleaning comment format

--- 一星題/10055/10055.cpp
+++ 一星題/10055/10055.cpp
@@ -3,11 +3,11 @@
 using namespace std; 
 int main()
 {
-  long long int Hashmat, opponent;// 宣告兩個長整數 Hashmat、opponent。因為資料的大小有可能超過 2^32，所以要用 long long int 存取。
+  long long int Hashmat, opponent; // 宣告兩個長整數 Hashmat、opponent。因為資料的大小有可能超過 2^32，所以要用 long long int 存取。
   /* 用 while 迴圈讓使用者連續輸入 */
   while(cin>>Hashmat>>opponent)
   {
-    cout<<abs(Hashmat-opponent)<<endl;// 數出 Hashmat 和 opponent 差的絕對值
+    cout<<abs(Hashmat-opponent)<<endl; // 數出 Hashmat 和 opponent 差的絕對值
   }
 }
 
