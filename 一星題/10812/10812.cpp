debe2a37b661c4c198f7bcaebb03b4b3fd70959c
Pigeon <32315294+LuckyPigeon@users.noreply.github.com>
2020-10-18 23:21:33 +0800

    Update 10812.cpp

--- 一星題/10812/10812.cpp
+++ 一星題/10812/10812.cpp
@@ -1,4 +1,3 @@
-https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1753
 #include <iostream> 
 using namespace std; 
 int main() 
@@ -9,11 +8,11 @@ int main()
     宣告整數 d，用來紀錄兩數之差。
   */
   int n, s, d; 
-  cin>>n;// 輸入有幾筆資料
+  cin>>n; // 輸入有幾筆資料
   /* 逐一處理每筆資料 */
   for(int i=0; i<n; i++)
   {
-    cin>>s>>d;// 輸入兩數之和、兩數之差
+    cin>>s>>d; // 輸入兩數之和、兩數之差
     /* 如果兩數之和大於兩數之差，且 s+d 除以 2 的餘數是 0，且 s-d 除以 2 的餘數是 0，就是有可能的資料 */
     if(s>d && ((s+d)%2==0) && ((s-d)%2==0))
       cout<<(s+d)/2<<" "<<(s-d)/2<<endl; // 輸出 s+d 除以 2 和 s-d 除以 2
