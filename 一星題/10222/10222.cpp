ac854955fa5b912cd0a742edfe7fcd5940b3dd29
LCH030 <61650214+LCH030@users.noreply.github.com>
2020-10-23 16:55:20 +0800

    Update 10222.cpp
    新增註解

--- 一星題/10222/10222.cpp
+++ 一星題/10222/10222.cpp
@@ -5,28 +5,32 @@ https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=14&pa
 using namespace std;
 int main()
 {
- const char keyboard[] = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
- const int kblength = strlen( keyboard );
- string s;
-  while(getline( cin, s ))
-  {
-   for( int i = 0 ; i < s.length() ; i++ )
+   const char keyboard[] = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./"; // 建立比對用的字串
+   const int kblength = strlen( keyboard ); // 宣告 int 變數 kblength ，存 keyboard 的長度
+   string s;
+   while(getline( cin, s )) // 當有輸入就執行 while 迴圈，且 getline 為整行輸入（包含空格）
    {
-    if(s[i] == ' ')
-      cout << " ";
-     else
-     {
-      for( int j = 0 ; j < kblength ; j++ )
+      for( int i = 0 ; i < s.length() ; i++ ) //執行 for 迴圈字串長度的次數，得以每個字 decode
       {
-       if( keyboard[j] == tolower(s[i]))
-       {
-        cout << ("%c", keyboard[j-2]);
-        break;
-       }
+         if(s[i] == ' ') // 這個 if 做的功能是空格保留
+            cout << " "; 
+         else
+         {
+            for( int j = 0 ; j < kblength ; j++ ) 
+            {
+               if( keyboard[j] == tolower(s[i])) 
+                  {
+                     cout << ("%c", keyboard[j-2]);
+                     break;
+                  }
+            }
+            /*
+            這個 for 迴圈做的功能是比對 s[i] 在 keyboard 的第幾個位置，得以找出左邊兩個按鍵的字母。
+            tolower function 功能是將大寫 char 變成小寫
+            */
+         }
+       }   
+       cout << "\n";
     }
-    }
-  }   
-  cout << "\n";
-  }
- return 0;
+    return 0;
 }
