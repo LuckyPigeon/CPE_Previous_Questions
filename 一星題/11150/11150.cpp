f52a84e7635d20d421ea2da5006abdbd6ea1df95
LCH030 <61650214+LCH030@users.noreply.github.com>
2020-10-23 15:12:31 +0800

    Update 11150.cpp
    增加註解

--- 一星題/11150/11150.cpp
+++ 一星題/11150/11150.cpp
@@ -3,26 +3,23 @@ https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&probl
 using namespace std;
 int main()
 {
-  int n, cola, sum;
-  while(cin>>n)
+  int n, cola, sum; // 宣告三個 int 變數 n cola sum
+  while(cin>>n) // 當輸入 n 執行 while 迴圈
   {
-    if(1<=n && n<=200)
+    if(1<=n && n<=200) // 如果小於 1 、大於 200 ，執行此 if
     {
-      cola=n;
-      while(n>=3)
+      cola=n; // assign n 得值給 cola
+      while(n>=3) // 當 n 大於等於 3 ，執行此 while 迴圈
       {
-        sum=n/3;
-        cola+=sum;
-        n=n%3+sum;
+        sum=n/3; // 3 瓶換 1 瓶，sum 存可以換到瓶子數的值
+        cola+=sum; // 將可以換到的瓶子數加進去 cola
+        n=n%3+sum; // n%3 為 3 瓶換 1 瓶以外的瓶子數，再將 n%3 加上 sum ，assign 給 n ， n 為換完之後“換得的”和“剩下的”瓶數
       }
       if(n==2)
-        cola++;
+        cola++; // 如果 n 等於 2 代表可以借一個空瓶換一瓶，喝完再還回去，所以 cola 可以加 1
       cout<<cola<<endl;
     }
   }
 }
 
 
-
-���d�ߨ�LCPE�D�ئp�U��
-http://jennaweng0621.pixnet.net/blog/category/6372301
