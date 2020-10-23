8eadc50b97a97cd508c4ef825dfa07634430403f
EXun <58657914+eeeXun@users.noreply.github.com>
2020-10-24 00:25:41 +0800

    Update 10235.cpp
    comment 10235

--- 一星題/10235/10235.cpp
+++ 一星題/10235/10235.cpp
@@ -5,7 +5,7 @@ const int LIMIT = 1000000;
 int reverse( int a )
 {
  int b = 0;
-  while( a != 0 )
+  while( a != 0 ) // 讓 b 存入 a 的最小位後,並且在每次存入前讓質乘以 10 ,以讓數字反轉
  {
    b *= 10;
    b += a%10;
@@ -15,10 +15,10 @@ int reverse( int a )
 }
 int main()
 {
- bool composite[LIMIT+5] = {true, true};
+ bool composite[LIMIT+5] = {true, true}; // 宣告 composite 陣列來紀錄是否為合數
   for( int i = 2; i <= LIMIT ; ++i )
  {
-   if( !composite[i] )
+   if( !composite[i] ) // 如果 i 不是合數 , 則去尋找它的倍數,並且把它的倍數紀錄為合數
   {
      for( int j = i+i ; j <= LIMIT ; j += i )
    {
@@ -27,17 +27,17 @@ int main()
    }
   }
   int N;
-  while( scanf("%d", &N) != EOF )
+  while( scanf("%d", &N) != EOF ) // while 輸入測資 N
  {
-   if( composite[N] )
+   if( composite[N] ) // 如果 N 是合數,則輸出 is not prime
   {
     cout << N << " is not prime." << endl;
     continue;
    }
-   int reverseN = reverse(N);
-   if( reverseN!=N && !composite[reverseN] )
+   int reverseN = reverse(N); // 反轉 N 的質,並存入 reverseN
+   if( reverseN!=N && !composite[reverseN] ) // 如果反轉後的 reverseN 與反轉前的 N 不相等,並且 reverseN 為質數,則輸出 is emirp
      cout << N << " is emirp." << endl;
-   else
+   else // 
     cout << N << " is prime." << endl;
   }
  return 0;
