ce00dbfcd5850e8ebc301fb8bb185cd4124de8a5
秋分 <51090706+chofinn@users.noreply.github.com>
2020-10-27 22:00:43 +0800

    Update 10050.cpp
    新增 10050 註解

--- 一星題/10050/10050.cpp
+++ 一星題/10050/10050.cpp
@@ -1,4 +1,4 @@
-https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=991
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=991
 
 #include <iostream>
 using namespace std;
@@ -14,18 +14,18 @@ int main() {
   ans = 0;
   noWork = new int [political];
   eachDays = new bool [day];
-  for(int i = 0; i < day; i++)
+  for(int i = 0; i < day; i++) // 先將所有日子預設為沒有罷工
    eachDays[i] = false;
 
   for(int i = 0; i < political; i++)
    cin >> noWork[i];
 
   for(int i = 0; i < political; i++)
-   for(int j = noWork[i] - 1; j < day; j += noWork[i])
+   for(int j = noWork[i] - 1; j < day; j += noWork[i]) // 每隔 noWork[i] 天就罷工
     eachDays[j] = true;
 
   for(int i = 0; i < day; i++)
-   if(eachDays[i] && i % 7 != 5 && i % 7 != 6)
+   if(eachDays[i] && i % 7 != 5 && i % 7 != 6) // eachDays[i] == true 就罷工，但星期五跟六不罷工
     ans++;
 
   delete [] noWork;
