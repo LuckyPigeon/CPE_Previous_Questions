56246cda1731789885478f81b0330959d566e2f2
秋分 <51090706+chofinn@users.noreply.github.com>
2020-10-31 16:07:44 +0800

    Update 10420.cpp
    新增 10420 註解

--- 一星題/10420/10420.cpp
+++ 一星題/10420/10420.cpp
@@ -1,4 +1,4 @@
-https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1361
+// https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1361
 
 #include<iostream>
 #include<map>
@@ -9,16 +9,16 @@ int main(){
  string first_s;
  char others[76]={0};
  int n;
- cin>>n;
+ cin>>n; // 總共將會有 n 行資料
  cin.ignore();
  while (n--){
-   cin>>first_s;
-   count[first_s]++;
-   cin.getline(others,76);
+   cin>>first_s; // 將國家名字讀進來
+   count[first_s]++; // 將此國家的出現次數 +1
+   cin.getline(others,76); // 讀掉後面不需要的資訊
  }
 for (iter=count.begin(); iter!=count.end();iter++){
-   cout<<iter->first<<" ";
-   cout<<iter->second<<endl;
+   cout<<iter->first<<" "; // 印出國家名
+   cout<<iter->second<<endl; // 印出對應數量
  }
  return 0;
 }
