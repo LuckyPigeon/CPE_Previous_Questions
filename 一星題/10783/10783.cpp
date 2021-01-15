930b885b6de9ce5afc004abac7615eb0de1499bc
YuXuan1216 <77137768+YuXuan1216@users.noreply.github.com>
2021-01-16 02:54:06 +0800

    Update 10783.cpp (#87)
    新增註解

--- 一星題/10783/10783.cpp
+++ 一星題/10783/10783.cpp
@@ -4,22 +4,22 @@ using namespace std;
 int main()
 {
  int T,a,b,time=0;
- cin>>T;
- while(T--)
+ cin>>T;  //輸入總case數
+ while(T--)  //當T不為0時，繼續執行
  {
-  cin>>a>>b;
-  time++;
-  int sum=0;
+  cin>>a>>b;  //輸入a,b值
+  time++;  //每進一次while迴圈次數+1
+  int sum=0;  //初始化sum，用來計算結果
   for(a;a<=b;a++)
   {
    if(a%2==1)
-    sum+=a;
+    sum+=a;  //當a為奇數，a加進sum
   }
-  cout<<"Case "<<time<<": "<<sum<<endl;
+  cout<<"Case "<<time<<": "<<sum<<endl;  //輸出當次case是第幾次、結果
  }
 }
 
-���d�ߩҦ��D�ؤθѵ��p�U��
+“琩高┮Τ肈ヘの秆氮“
 https://jennaweng0621.pixnet.net/blog/post/403629785
-���d�ߨ�LCPE�D�ئp�U��
+“琩高ㄤCPE肈ヘ“
 http://jennaweng0621.pixnet.net/blog/category/6372301
