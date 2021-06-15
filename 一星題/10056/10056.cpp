5206f9220649f93735f7f3f483cdfa2c1da93f1b
Ting-Jun Wang <levi900227@gmail.com>
2021-06-15 17:06:53 +0800

    Comment out URL

--- 一星題/10056/10056.cpp
+++ 一星題/10056/10056.cpp
@@ -1,4 +1,4 @@
-https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=997
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=997
 
 #include <iostream>
 #include <math.h>
